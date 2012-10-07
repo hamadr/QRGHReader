d2235ab936ba5556928dafb263f68d0301488143
hamadr <hamadr@hamadr-desktop>
2012-10-07 15:43:22 +0200

    oups

--- QRGHReader/src/com/ensimag/securimag/qrghreader/MainActivity.java
+++ QRGHReader/src/com/ensimag/securimag/qrghreader/MainActivity.java
@@ -19,7 +19,6 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
-import com.ensimag.securimag.qrghreader.Base64;
 
 public class MainActivity extends Activity {
 	private Camera mCamera;
